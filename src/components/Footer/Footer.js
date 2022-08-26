8b5b586d704a006f7aa7ae9d6e06f9de3c7d95c9
Favour Ifeanacho <favour.franki3@gmail.com>
2022-08-27 00:19:34 +0100

    deployment for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import MobileStore from '../Mobile/MobileStore'
 import './footer.models.css'
 
 const Footer = () => {
   return (
     <footer>
-        
+        <div className='footer-main'>
+            <div>
+                <h5>Company</h5>
+                <p>Career</p>
+                <p>About Us</p>
+                <p>Contact Us</p>
+            </div>
+            <div>
+                <h5>Support</h5>
+                <p>Help Center</p>
+                <p>Safety Center</p>
+            </div>
+            <div>
+                <h5>Legal</h5>
+                <p>Privacy Policy</p>
+                <p>Cookies Policy</p>
+                <p>Terms of Service</p>
+                <p>Dispute resolution</p>
+            </div>
+            <div>
+                <h5>Install App</h5>
+                <MobileStore />
+            </div>
+        </div>
     </footer>
   )
 }
