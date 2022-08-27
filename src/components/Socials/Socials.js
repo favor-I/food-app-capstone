4a3dc25a9375ade072e92f347d4eeacaf13a57b5
Favour Ifeanacho <favour.franki3@gmail.com>
2022-08-27 02:31:10 +0100

    deployment for complete footer section

---
+++ src/components/Socials/Socials.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import instagram from './instagram.svg'
+import twitter from './twitter.svg'
+import youTube from './youTube.svg'
+
+const Socials = () => {
+  return (
+    <>
+        <img src={instagram} alt='instagram icon'></img>
+        <img src={twitter} alt='twitter icon'></img>
+        <img src={twitter} alt='twitter icon'></img>
+    </>
+  )
+}
+
+export default Socials
