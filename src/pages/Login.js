87bea2fd10a48f8f9f85101f5185286d7c746530
Favour Ifeanacho <favour.franki3@gmail.com>
2022-09-02 23:10:29 +0100

    deployment fix for last failed build(an issue around unused state)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,7 +10,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [loginData, setLoginData] = useState({})
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  // const [isAuthenticated, setIsAuthenticated] = useState(false)
   const navigate = useNavigate()
 
 
@@ -26,7 +26,7 @@ const Login = () => {
     const storedUser = JSON.parse(sessionStorage.getItem('newUser'))
     
     if(loginData?.userEmail === storedUser?.userEmail && loginData?.userPassword === storedUser?.userPassword ){
-      setIsAuthenticated(true)
+      // setIsAuthenticated(true)
       NotificationManager.success(`${storedUser.userName} is signed in`, 'Success Message', 2000)
       navigate('/dashboard')
     }else{
